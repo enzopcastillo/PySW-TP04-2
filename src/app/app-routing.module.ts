8949810b3fa02f1fd63b5953f192fd4eaa70d15c
enzopcastillo <epcastillov@gmail.com>
2022-05-13 23:38:13 -0300

    Angular, 2da parte.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { Page1Component } from './components/pages/page1/page1.component';
+import { PageaComponent } from './components/pages/pagea/pagea.component';
+import { PagebComponent } from './components/pages/pageb/pageb.component';
+import { PagecComponent } from './components/pages/pagec/pagec.component';
+
+const routes: Routes = [
+  {path: 'page1', component: Page1Component},
+  {path: 'pagea', component: PageaComponent},
+  {path: 'pageb', component: PagebComponent},
+  {path: 'pagec', component: PagecComponent},
+  {path: '**', pathMatch: 'full', redirectTo: 'pagea'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/layout/header/header.component';
+import { FooterComponent } from './components/layout/footer/footer.component';
+import { PageaComponent } from './components/pages/pagea/pagea.component';
+import { PagebComponent } from './components/pages/pageb/pageb.component';
+import { PagecComponent } from './components/pages/pagec/pagec.component';
+import { Page1Component } from './components/pages/page1/page1.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    PageaComponent,
+    PagebComponent,
+    PagecComponent,
+    Page1Component
   ],
   imports: [
     BrowserModule,
